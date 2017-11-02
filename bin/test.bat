f2ee49a353e5be851928c7dbf67260ebe9a51ee0
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2017-11-02 18:20:28 -0400

    oosu script will download latest configuration from github master

--- bin/test.bat
+++ bin/test.bat
@@ -2,7 +2,8 @@
 cd /d "%~dp0"
 REM lets wait for 90 seconds to reduce the resource impact on startup
 ping localhost -n 91 >nul
-echo %time%>>testtest.txt
-REM "%~dp0\wget" -N https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe
+REM echo %time%>>testtest.txt
+"%~dp0\wget" -N https://raw.githubusercontent.com/thaatz/w10lite/master/bin/ooshutup10.cfg
+"%~dp0\wget" -N https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe
 OOSU10.exe ooshutup10.cfg /quiet /nosrp
 pause
