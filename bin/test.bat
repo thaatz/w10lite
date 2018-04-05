7dbff9a4dcf6293c78a000c34827514aa70dd246
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2018-04-05 14:44:22 -0400

    added master config setting
    added a setting that would notify the user when oosu10.exe updated so that changes or updates could be made for w10lite upates. Also changed EOL to CR LF to address some syntax bugs.

--- bin/test.bat
+++ bin/test.bat
@@ -3,22 +3,45 @@ REM cd /d "%~dp0"
 pushd "%~dp0" 2>NUL
 call :read_settings w10lite.ini
 
-REM determine the config we're gonna use
+:: determine the config we're gonna use
 if %config%==tom (set cfg=ooshutup10tom.cfg
+	) else if %config%==master (set cfg=ooshutup10tom.cfg
 	) else if %config%==bare (set cfg=ooshutup10bare.cfg
-	) else echo "config" in w10lite.ini is not properly set. check your config
+	) else (
+		echo "config" in w10lite.ini is not properly set. check your config
+		pause
+		exit
+		)
 
-REM legacy boot mode stuff
+:: legacy boot mode stuff
 if %safemode%==legacy (bcdedit /set {default} bootmenupolicy legacy
 	) else if %safemode%==modern (echo>nul
-	) else echo "safemode" in w10lite.ini is not properly set. check your config
+	) else (
+		echo "safemode" in w10lite.ini is not properly set. check your config
+		pause
+		exit
+		)
 
 :: lets wait for 90 seconds to reduce the resource impact on startup
 ping localhost -n 91 >nul
-REM echo %time%>>testtest.txt
 wget -N https://raw.githubusercontent.com/thaatz/w10lite/master/bin/%cfg%
-wget -N https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe
+:: we cant pipe the output of wget, so we make a log file instead
+wget -N https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe -o testlog.txt
+if %config%==master (
+	find /i "saved" testlog.txt >nul
+	REM echo errorlevel %errorlevel%
+	if %errorlevel%==0 (
+		msg * OOSU10 was updated!
+		)
+	)
+:: some other interesting message ideas that i did not use because the script will run as system, not user
+:: but i might need this some other day
+:: https://stackoverflow.com/questions/12514475/how-can-you-create-pop-up-messages-in-a-batch-script
+
+::delete the wget log file
+del testlog.txt
 OOSU10.exe %cfg% /quiet /nosrp
+exit
 
 :read_settings
 set SETTINGSFILE=%1
