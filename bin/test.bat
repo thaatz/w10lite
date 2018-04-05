fe8482907c8a5d2ed62af052d6ad2e30618f015d
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2018-04-05 02:51:35 -0400

    added configuration file
    Added configuration file that has a custom oosu profile for time and a generic bare bones setting suitable for most computers. Also added a setting that would re-enable legacy safemode booting via the f8 key in windows 10

--- bin/test.bat
+++ bin/test.bat
@@ -1,8 +1,33 @@
 @echo off
-cd /d "%~dp0"
+REM cd /d "%~dp0"
+pushd "%~dp0" 2>NUL
+call :read_settings w10lite.ini
+
+REM determine the config we're gonna use
+if %config%==tom (set cfg=ooshutup10tom.cfg
+	) else if %config%==bare (set cfg=ooshutup10bare.cfg
+	) else echo "config" in w10lite.ini is not properly set. check your config
+
+REM legacy boot mode stuff
+if %safemode%==legacy (bcdedit /set {default} bootmenupolicy legacy
+	) else if %safemode%==modern (echo>nul
+	) else echo "safemode" in w10lite.ini is not properly set. check your config
+
 :: lets wait for 90 seconds to reduce the resource impact on startup
 ping localhost -n 91 >nul
 REM echo %time%>>testtest.txt
-wget -N https://raw.githubusercontent.com/thaatz/w10lite/master/bin/ooshutup10.cfg
+wget -N https://raw.githubusercontent.com/thaatz/w10lite/master/bin/%cfg%
 wget -N https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe
-OOSU10.exe ooshutup10.cfg /quiet /nosrp
+OOSU10.exe %cfg% /quiet /nosrp
+
+:read_settings
+set SETTINGSFILE=%1
+if not exist %SETTINGSFILE% (
+	echo w10lite.ini does not exist
+	pause
+    exit
+)
+for /f "eol=# delims== tokens=1,2" %%i in (%SETTINGSFILE%) do (
+    set %%i=%%j
+)
+exit /b 0
