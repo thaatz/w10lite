6db944f7936a390fe72b60a59f0302c9cc202606
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2023-12-19 12:44:02 -0500

    script will now wait for oosu10 to finish
    without the wait the second run will skip if the first is still in progress

--- runnow.bat
+++ runnow.bat
@@ -8,7 +8,10 @@ bcdedit /set {default} bootmenupolicy legacy
 curl.exe -s --insecure "https://raw.githubusercontent.com/ChrisTitusTech/winutil/main/ooshutup10_winutil_settings.cfg" -o ooshutup10.cfg
 rem curl.exe -s --insecure "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -o OOSU10.exe
 choco upgrade shutup10 -y
-oosu10.exe ooshutup10.cfg /quiet
+rem we use start /wait (alternatively we could use cmd /c) because if it executes too quickly the subsequent runs wont go
+rem sometimes start /wait wont work when using the commandline shim for oosu10
+start "" /wait %programdata%\chocolatey\lib\shutup10\tools\oosu10.exe ooshutup10.cfg /quiet
+@REM oosu10.exe ooshutup10.cfg /quiet
 oosu10.exe ooshutup10tom.cfg /quiet
 popd
 exit /b 0
